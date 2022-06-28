a39b4ad163231d8082b47565d47c4a20591fd0d4
unknown <yleecruz@gmail.com>
2022-06-28 11:12:46 -0400

    header icons fix

--- pages/index.js
+++ pages/index.js
@@ -15,13 +15,14 @@ export default function Home() {
 			</Head>
 
 			<header className="flex flex-row  items-center  px-2  h-10    text-slate-500">
-				<i className=" flex-1 fas fa-bars"></i>
+				<i className=" flex-1 las la-bars text-2xl"></i>
 				<div className="flex items-center ">
-					<i className="fas fa-bag-shopping pr-4 text-xl">
-						<span className="absolute -ml-2 mb-4 w-4  h-4 text-center text-white rounded-full bg-red-500 font-thin text-xs  ">
-							2
+					<div className="flex relative mr-4 pt-1">
+						<i className="las la-shopping-cart text-2xl  cursor-pointer"></i>
+						<span className="absolute text-center justify-center text-xs h-4 w-4 ml-3    text-white     rounded-full  bg-red-500">
+							0
 						</span>
-					</i>
+					</div>
 
 					<div className="rounded-full justify-end   h-8 w-8 bg-red-100"></div>
 				</div>
