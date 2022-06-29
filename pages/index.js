3e797da3a10b2ce8dc5b38dface15fa242fbc8af
unknown <yleecruz@gmail.com>
2022-06-29 10:23:30 -0400

    some css fix 4

--- pages/index.js
+++ pages/index.js
@@ -26,6 +26,19 @@ export default function Home() {
 			<ProductsList products={ProductsListData} />
 			<BookTableDialog />
 
+      <section className="flex m-2 p-4 bg-gray-700  items-center   h-48  rounded-xl shadow-lg   ">
+				<div className=" text-center p-4 ">
+					<h3 className=" mb-2  font-bold text-slate-200">Happy Hour</h3>
+					<p className=" text-sm text-slate-300 text-center items-center flex">
+						<i className="las la-map-marker-alt text-xl mx-2  text-red-500"></i>13855 W hillsborough
+						Ave Tampa, Fl 33635
+					</p>
+					<p className="text-slate-200 text-sm items-center flex justify-center">
+						<i className="las la-phone text-xl mx-2 text-center  text-red-500"></i>(813) 475-4493
+					</p>
+				</div>
+			</section>
+
 			<section
 				name="socials"
 				className=" m-2 p-4 bg-gray-50  items-center   h-32  rounded-xl shadow-lg"
