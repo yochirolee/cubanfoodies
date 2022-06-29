3ee37137e689bc0bfd98fc0278c99b913a428bed
unknown <yleecruz@gmail.com>
2022-06-29 10:05:58 -0400

    some css fix 2

--- components/dialogs/bookTableDialog.jsx
+++ components/dialogs/bookTableDialog.jsx
@@ -10,23 +10,7 @@ export const BookTableDialog = () => {
 					<div className="relative bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
 						<div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
 							<div className="sm:flex sm:items-start">
-								<div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-									<svg
-										className="h-6 w-6 text-red-600"
-										xmlns="http://www.w3.org/2000/svg"
-										fill="none"
-										viewBox="0 0 24 24"
-										strokeWidth="2"
-										stroke="currentColor"
-										aria-hidden="true"
-									>
-										<path
-											stroke-linecap="round"
-											stroke-linejoin="round"
-											d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-										/>
-									</svg>
-								</div>
+								<div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"></div>
 								<div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
 									<h3 className="text-lg leading-6 font-medium text-gray-900" id="modal-title">
 										Deactivate account
