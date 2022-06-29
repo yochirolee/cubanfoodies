751ae04c58a3548bfeeb2c38fd7daa6adaebcc0b
unknown <yleecruz@gmail.com>
2022-06-29 10:05:10 -0400

    some css fix 2

--- components/dialogs/bookTableDialog.jsx
+++ components/dialogs/bookTableDialog.jsx
@@ -16,7 +16,7 @@ export const BookTableDialog = () => {
 										xmlns="http://www.w3.org/2000/svg"
 										fill="none"
 										viewBox="0 0 24 24"
-										stroke-width="2"
+										strokeWidth="2"
 										stroke="currentColor"
 										aria-hidden="true"
 									>
